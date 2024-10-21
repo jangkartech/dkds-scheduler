59a5875c0cbafddf4330c3ce3aea4e1c829951ef
bayu-jti <bayu@jangkar.io>
2024-10-21 12:05:30 +0800

    create function for get detail order

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import {getOrders} from "./services/dkds.service.js";
+import {getOrders, getDetailOrder} from "./services/dkds.service.js";
 
 const main  = async () => {
-    const orders = await  getOrders();
-    console.log(orders);
+    // const orders = await  getOrders();
+    // console.log(orders);
+    const orderId = '202210030001';
+    const order = await getDetailOrder(orderId);
+    console.log(order);
 }
 
 main().then();
--- services/dkds.service.js
+++ services/dkds.service.js
@@ -68,3 +68,50 @@ export const getOrders = async () => {
         console.error('Error fetching orders:', error);
     }
 };
+
+export const getDetailOrder = async (orderId) => {
+    try {
+        // Read and parse the credentials JSON file
+        const credentialsPath = path.resolve('./credential.json');
+        const credentialsData = await fs.readFile(credentialsPath, 'utf-8');
+        const credentials = JSON.parse(credentialsData);
+
+        const csrf = credentials.DKDS_CSRF;
+        const cookie = credentials.DKDS_COOKIE;
+        const baseUrl = process.env.DKDS_BASE_URL;
+
+        console.log({
+            'csrf': csrf,
+            'cookie': cookie,
+            'baseUrl': baseUrl
+        })
+        // Perform the POST request
+        const response = await fetch(baseUrl + '/dkds/web/index.php?r=sfa%2Freal-order%2Fview&id='+orderId+'&distributor_code=70006022', {
+            method: 'POST',
+            headers: {
+                'Cookie': cookie,
+                'Accept': 'text/plain, */*; q=0.01',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': '192.168.1.253',
+                'Origin': baseUrl,
+                'Referer': baseUrl + '/dkds/web/index.php?r=sfa%2Freal-order%2Fmultiple',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+        });
+
+        const contentType = response.headers.get('content-type');
+
+        if (contentType && contentType.includes('application/json')) {
+            return await response.json()
+        } else {
+           const text =  await response.text();
+           return JSON.parse(text);
+        }
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+    }
+}
