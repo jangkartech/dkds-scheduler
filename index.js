78d8cf6d4dd8fdcaa2ca6629774b753486a7c362
bayu-jti <bayu@jangkar.io>
2024-10-29 15:04:42 +0800

    add get sales order by id

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import {JSDOM} from "jsdom";
-import {getDKDSSaleOrders, refetchCookies} from "./services/dkds.service.js";
+import {
+    getAllDKDSSaleOrders,
+    getDKDSSaleOrders,
+    getDKDSSaleOrdersById,
+    refetchCookies
+} from "./services/dkds.service.js";
 
 const main = async () => {
     // console.log(orders);
@@ -20,81 +25,11 @@ const main = async () => {
     // console.log(order);
 
     await refetchCookies();
-    let allData = [];
-    let currentPage = 1; // Start from the first page
-
-    const fetchAndScrapeData = async (page) => {
-        const orders = await getDKDSSaleOrders(page);
-        const {document} = (new JSDOM(orders)).window;
-        const rows = document.querySelectorAll('.table tbody tr');
-
-        const scrapedData = Array.from(rows).map(row => {
-            const cells = row.querySelectorAll('td');
-            const salesman = cells[4].textContent.trim().split(' - ');
-            const customer = cells[5].textContent.trim().split(' - ');
-            const customerNameAndAddress = customer[1].split('Add: ');
-
-            return {
-                rowNumber: cells[0].textContent.trim(),
-                salesOrder: cells[1].textContent.trim(),
-                noRealOrder: cells[2].textContent.trim(),
-                date: cells[3].textContent.trim(),
-                salesmanCode: salesman[0],
-                salesmanName: salesman[1],
-                customerCode: customer[0],
-                customerName: customerNameAndAddress[0].trim(),
-                customerAddress: customerNameAndAddress[1].trim() || '',
-                totalInv: cells[6].textContent.trim(),
-                description: cells[7].textContent.trim(),
-                postedBy: cells[8].textContent.trim(),
-                postStatus: cells[9].textContent.trim(),
-                blockOrder: cells[10].textContent.trim()
-            };
-        });
-
-        // Initialize pagination object
-        const pagination = {
-            currentPage: null,
-            totalPage: 0,
-            total: 0,
-            pages: []
-        };
-
-        // Find all page links
-        const pageLinks = [...document.querySelectorAll('.pagination li a')]
-            .filter(link => !['First', 'Last', '«', '»'].includes(link.textContent));
-
-        pagination.totalPage = pageLinks.length;
-
-        const activePage = document.querySelector('.pagination li.active a');
-        if (activePage) {
-            pagination.currentPage = parseInt(activePage.textContent, 10);
-        }
-
-        pagination.pages = pageLinks.map(link => parseInt(link.textContent, 10));
-        pagination.total = scrapedData.length;
-
-        return {scrapedData, pagination};
-    };
-
-// Fetch and scrape data in a loop
-    let hasMorePages = true;
-    while (hasMorePages) {
-        const {scrapedData, pagination} = await fetchAndScrapeData(currentPage);
-        console.log({data: scrapedData, meta: {pagination}});
-
-        allData = allData.concat(scrapedData);
-
-        // Check if there are more pages to process
-        if (pagination.currentPage < pagination.totalPage) {
-            currentPage++; // Move to the next page
-        } else {
-            hasMorePages = false; // No more pages
-        }
-    }
-
-// Now allData contains data from all pages
-    console.log(allData);
+    const orders = await getAllDKDSSaleOrders();
+    // Now orders contains data from all pages
+    console.log(orders);
+    const order = await getDKDSSaleOrdersById(orders[0].id);
+    console.log(order);
 };
 
 main().then();
--- services/dkds.service.js
+++ services/dkds.service.js
@@ -10,24 +10,24 @@ config();
 
 export const refetchCookies = async () => {
     const baseUrl = process.env.DKDS_BASE_URL;
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     // Navigate to the login page
-    await page.goto(baseUrl+'/dkds/web/index.php?r=site%2Flogin', { waitUntil: 'networkidle2' });
+    await page.goto(baseUrl + '/dkds/web/index.php?r=site%2Flogin', {waitUntil: 'networkidle2'});
 
     // Wait for the form fields to be visible
     await page.waitForSelector('#loginform-username');
     await page.waitForSelector('#loginform-password');
 
     // Fill in the username and password from .env
-    await page.type('#loginform-username', process.env.DKDS_USERNAME, { delay: 100 });
-    await page.type('#loginform-password', process.env.DKDS_PASSWORD, { delay: 100 });
+    await page.type('#loginform-username', process.env.DKDS_USERNAME, {delay: 100});
+    await page.type('#loginform-password', process.env.DKDS_PASSWORD, {delay: 100});
 
     // Submit the form
     await Promise.all([
         page.click('button[type="submit"]'),  // Click the login button
-        page.waitForNavigation({ waitUntil: 'networkidle2' })  // Wait for the next page to load
+        page.waitForNavigation({waitUntil: 'networkidle2'})  // Wait for the next page to load
     ]);
 
     // Extract the CSRF token from the dashboard
@@ -68,7 +68,7 @@ export const getOrders = async () => {
         const csrf = credentials.DKDS_CSRF;
         const csrfCookie = credentials.COOKIES[0].value;
         const phpsessid = credentials.COOKIES[1].value;
-        const cookie = "PHPSESSID="+phpsessid+"; _csrf="+csrfCookie;
+        const cookie = "PHPSESSID=" + phpsessid + "; _csrf=" + csrfCookie;
         const baseUrl = process.env.DKDS_BASE_URL;
 
         console.log({
@@ -132,7 +132,7 @@ export const getDetailOrder = async (orderId) => {
         const csrf = credentials.DKDS_CSRF;
         const csrfCookie = credentials.COOKIES[0].value;
         const phpsessid = credentials.COOKIES[1].value;
-        const cookie = "PHPSESSID="+phpsessid+"; _csrf="+csrfCookie;
+        const cookie = "PHPSESSID=" + phpsessid + "; _csrf=" + csrfCookie;
         const baseUrl = process.env.DKDS_BASE_URL;
 
         console.log({
@@ -152,7 +152,6 @@ export const getDetailOrder = async (orderId) => {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'Host': '192.168.1.253',
                 'Origin': baseUrl,
-                'Referer': baseUrl + '/dkds/web/index.php?r=sfa%2Freal-order%2Fmultiple',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
                 'X-Requested-With': 'XMLHttpRequest'
             },
@@ -241,7 +240,7 @@ export const getDKDSSaleOrders = async (page = 1) => {
         const csrf = credentials.DKDS_CSRF;
         const csrfCookie = credentials.COOKIES[0].value;
         const phpsessid = credentials.COOKIES[1].value;
-        const cookie = "PHPSESSID="+phpsessid+"; _csrf="+csrfCookie;
+        const cookie = "PHPSESSID=" + phpsessid + "; _csrf=" + csrfCookie;
         const baseUrl = process.env.DKDS_BASE_URL;
 
         console.log({
@@ -251,7 +250,7 @@ export const getDKDSSaleOrders = async (page = 1) => {
         })
 
         // Perform the POST request
-        const response = await fetch(baseUrl + '/dkds/web/index.php?r=transaksi/sales-order/index&TransaksiSorderSearch[id]=&TransaksiSorderSearch[no_rorder]=&TransaksiSorderSearch[date]=&TransaksiSorderSearch[sales_id]=&TransaksiSorderSearch[outlet_code]=&TransaksiSorderSearch[total_inv]=&TransaksiSorderSearch[keterangan]=&TransaksiSorderSearch[user_id]=&TransaksiSorderSearch[post]=1&TransaksiSorderSearch[block_order]=&page='+page, {
+        const response = await fetch(baseUrl + '/dkds/web/index.php?r=transaksi/sales-order/index&TransaksiSorderSearch[id]=&TransaksiSorderSearch[no_rorder]=&TransaksiSorderSearch[date]=&TransaksiSorderSearch[sales_id]=&TransaksiSorderSearch[outlet_code]=&TransaksiSorderSearch[total_inv]=&TransaksiSorderSearch[keterangan]=&TransaksiSorderSearch[user_id]=&TransaksiSorderSearch[post]=1&TransaksiSorderSearch[block_order]=&page=' + page, {
             method: 'GET',
             headers: {
                 'Cookie': cookie,
@@ -271,9 +270,131 @@ export const getDKDSSaleOrders = async (page = 1) => {
         if (contentType && contentType.includes('application/json')) {
             return await response.json()
         } else {
-            return  await response.text();
+            return await response.text();
         }
     } catch (error) {
         console.error('Error fetching orders:', error);
     }
 };
+
+export const getAllDKDSSaleOrders = async () => {
+    let allData = [];
+    let currentPage = 1; // Start from the first page
+
+    const fetchAndScrapeData = async (page) => {
+        const orders = await getDKDSSaleOrders(page);
+        const {document} = (new JSDOM(orders)).window;
+        const rows = document.querySelectorAll('.table tbody tr');
+
+        const scrapedData = Array.from(rows).map(row => {
+            const cells = row.querySelectorAll('td');
+            const salesman = cells[4].textContent.trim().split(' - ');
+            const customer = cells[5].textContent.trim().split(' - ');
+            const customerNameAndAddress = customer[1].split('Add: ');
+
+            return {
+                rowNumber: cells[0].textContent.trim(),
+                salesOrder: cells[1].textContent.trim(),
+                noRealOrder: cells[2].textContent.trim(),
+                date: cells[3].textContent.trim(),
+                salesmanCode: salesman[0],
+                salesmanName: salesman[1],
+                customerCode: customer[0],
+                customerName: customerNameAndAddress[0].trim(),
+                customerAddress: customerNameAndAddress[1].trim() || '',
+                totalInv: cells[6].textContent.trim(),
+                description: cells[7].textContent.trim(),
+                postedBy: cells[8].textContent.trim(),
+                postStatus: cells[9].textContent.trim(),
+                blockOrder: cells[10].textContent.trim()
+            };
+        });
+
+        // Initialize pagination object
+        const pagination = {
+            currentPage: null,
+            totalPage: 0,
+            total: 0,
+            pages: []
+        };
+
+        // Find all page links
+        const pageLinks = [...document.querySelectorAll('.pagination li a')]
+            .filter(link => !['First', 'Last', '«', '»'].includes(link.textContent));
+
+        pagination.totalPage = pageLinks.length;
+
+        const activePage = document.querySelector('.pagination li.active a');
+        if (activePage) {
+            pagination.currentPage = parseInt(activePage.textContent, 10);
+        }
+
+        pagination.pages = pageLinks.map(link => parseInt(link.textContent, 10));
+        pagination.total = scrapedData.length;
+
+        return {scrapedData, pagination};
+    };
+
+    // Fetch and scrape data in a loop
+    let hasMorePages = true;
+    while (hasMorePages) {
+        const {scrapedData, pagination} = await fetchAndScrapeData(currentPage);
+        console.log({data: scrapedData, meta: {pagination}});
+
+        allData = allData.concat(scrapedData);
+
+        // Check if there are more pages to process
+        if (pagination.currentPage < pagination.totalPage) {
+            currentPage++; // Move to the next page
+        } else {
+            hasMorePages = false; // No more pages
+        }
+    }
+    return allData;
+}
+
+export const getDKDSSaleOrdersById = async (id) => {
+    try {
+        // Read and parse the credentials JSON file
+        const credentialsPath = path.resolve('./credential.json');
+        const credentialsData = await fs.readFile(credentialsPath, 'utf-8');
+        const credentials = JSON.parse(credentialsData);
+
+        const csrf = credentials.DKDS_CSRF;
+        const csrfCookie = credentials.COOKIES[0].value;
+        const phpsessid = credentials.COOKIES[1].value;
+        const cookie = "PHPSESSID=" + phpsessid + "; _csrf=" + csrfCookie;
+        const baseUrl = process.env.DKDS_BASE_URL;
+
+        console.log({
+            'csrf': csrf,
+            'cookie': cookie,
+            'baseUrl': baseUrl
+        })
+
+        // Perform the POST request
+        const response = await fetch(baseUrl + '/dkds/web/index.php?r=transaksi/sales-order/view&distributor_code=70006022&id=' + id, {
+            method: 'GET',
+            headers: {
+                'Cookie': cookie,
+                'Accept': 'text/plain, */*; q=0.01',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': '192.168.1.253',
+                'Origin': baseUrl,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+        });
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+            return await response.json()
+        } else {
+            return await response.text();
+        }
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+    }
+}
