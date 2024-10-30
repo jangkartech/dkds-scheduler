de22161f3f6ebb50ef7a7b9ff633991b9a262470
bayu-jti <bayu@jangkar.io>
2024-10-30 16:04:03 +0800

    move the scrapping logic to service

--- services/dkds.service.js
+++ services/dkds.service.js
@@ -659,7 +659,57 @@ export const getDKDSSaleOrderItemsById = async (id, customerCode) => {
         if (contentType && contentType.includes('application/json')) {
             return await response.json()
         } else {
-            return  await response.text();
+            const items =  await response.text();
+            const {document} = (new JSDOM(items)).window;
+            const rows = Array.from(document.querySelectorAll('.table-items tr')); // Convert NodeList to array
+            const data = [];
+            rows.shift();
+            rows.splice(-2);
+            rows.forEach((row, index) => {
+                const getTextContent = (selector) => {
+                    const subRow = row.querySelector(selector);
+                    return subRow ? subRow.textContent.trim() : '';
+                };
+
+                const no = parseFloat(getTextContent('td:nth-child(1)').replace(/[,.-]/g, '')) || 0;
+                const productCode = getTextContent('td:nth-child(2)');
+                const productName = getTextContent('td:nth-child(3)');
+                const color = getTextContent('td:nth-child(5)');
+
+                const productQty = [];
+                for (let i = 6; i <= 10; i++) {
+                    const qtyText = getTextContent(`td:nth-child(${i})`).replace(/\t+/g, '').trim().replace(getTextContent(`td:nth-child(${i}) span`), '');
+                    productQty.push({
+                        qty: parseFloat(qtyText.replace(/[,.-]/g, '')) || 0, // Convert qty to float
+                        uom: getTextContent(`td:nth-child(${i}) span`),
+                    });
+                }
+
+                const productPrices = [];
+                for (let i = 11; i <= 15; i++) {
+                    const priceText = getTextContent(`td:nth-child(${i})`);
+                    productPrices.push(parseFloat(priceText.replace(/[,.-]/g, '')) || 0); // Convert prices to float
+                }
+
+                const amount = getTextContent('td:nth-child(16)');
+                const adjustmentPrice = getTextContent('td:nth-child(17)');
+                const discountAmount = getTextContent('td:nth-child(18)');
+
+                const item = {
+                    no,
+                    productCode,
+                    productName,
+                    color,
+                    productQty,
+                    productPrices,
+                    amount: parseFloat(amount.replace(/[,.-]/g, '')) || 0,
+                    adjustmentPrice: parseFloat(adjustmentPrice.replace(/[,.-]/g, '')) || 0,
+                    discountAmount: parseFloat(discountAmount.replace(/[,.-]/g, '') || 0)
+                };
+
+                data.push(item);
+            });
+            return data
         }
     } catch (error) {
         console.error('Error fetching orders:', error);
