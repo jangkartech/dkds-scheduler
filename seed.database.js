1ae1ab86dcdb9d6d7815ad53434efa3707926803
bayu-jti <bayu@jangkar.io>
2024-11-18 16:02:03 +0800

    fix more general seeder

--- seed.database.js
+++ seed.database.js
@@ -2,26 +2,18 @@ import { promises as fs } from 'fs';
 import XLSX from 'xlsx';
 import Mapper from './database/models/mapper.js'; // adjust path to your Mapper model
 
-const seed = async (filePath) => {
+const seedData = async (filePath, model) => {
   try {
     const fileBuffer = await fs.readFile(filePath);
 
-    // Parse the Excel file to JSON
     const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
-    const sheet = workbook.Sheets[workbook.SheetNames[0]]; // Assume first sheet
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const data = XLSX.utils.sheet_to_json(sheet);
 
-    // Create array of promises to insert data
     const promises = data.map(async (row) => {
-      // Map the data to your model
-      await Mapper.create({
-        id: row.id,
-        mappedEntityId: row.mappedEntityId,
-        entityType: row.entityType,
-      });
+      await model.create(row);
     });
 
-    // Wait for all insertions to complete
     await Promise.all(promises);
     console.log('Seeding completed successfully!');
   } catch (error) {
@@ -30,7 +22,7 @@ const seed = async (filePath) => {
 };
 
 const main = async () => {
-  await seed('./database/seeders/mapper.xlsx');
+  await seedData('./database/seeders/mapper.xlsx', Mapper);
 };
 
 main().then();
