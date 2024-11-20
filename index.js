379d7cfcc292624876a6bcc18d858fb9cfe89f43
bayu-jti <bayu@jangkar.io>
2024-11-20 12:08:35 +0800

    save the orders to local json

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {
   getDKDSSaleOrderItemsById,
   refetchCookies,
 } from './services/dkds.service.js';
+import { promises as fs } from 'fs';
 
 const main = async () => {
   // const twinUser = await refetchTwinToken();
@@ -14,15 +15,24 @@ const main = async () => {
   // console.log(order);
   await refetchCookies();
   const orders = await getAllDKDSSaleOrders();
+  const mergedOrders = [];
   for (const order of orders) {
     const orderDetail = await getDKDSSaleOrderById(order.noSalesOrder);
-    console.log(orderDetail);
+    // console.log(orderDetail);
     const orderItems = await getDKDSSaleOrderItemsById(
       order.noSalesOrder,
       order.customerCode
     );
-    console.dir(orderItems, { depth: null });
+    // console.dir(orderItems, { depth: null });
+    // Merge items into orderDetail
+    orderDetail.items = orderItems;
+    mergedOrders.push(orderDetail);
   }
+  await fs.writeFile(
+    'orders.json',
+    JSON.stringify(mergedOrders, null, 2),
+    'utf8'
+  );
 };
 
 main().then();
